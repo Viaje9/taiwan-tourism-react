5f1c66a19f5deb7ae111f384761ea13881f1fe24
miahsu <Mia.hsu@tpisoftware.com>
2022-05-14 18:05:10 +0800

    功能分配

--- src/view/Restaurant/RestaurantDetail/RestaurantDetail.jsx
+++ src/view/Restaurant/RestaurantDetail/RestaurantDetail.jsx
@@ -1,15 +1,15 @@
 // import { Detail } from '@/components/Detail/Detail.jsx';
-
+// TODO: Eric
 export default function RestaurantDetail() {
-    return (
-        <div>
-            RestaurantDetail
-            {/* <Detail
+  return (
+    <div>
+      RestaurantDetail
+      {/* <Detail
                 detailiID={id}
                 info={detailInfo}
                 category="restaurant"
                 categoryStr="餐飲"
             /> */}
-        </div>
-    )
+    </div>
+  )
 }
--- src/view/Restaurant/Restaurants/Restaurants.jsx
+++ src/view/Restaurant/Restaurants/Restaurants.jsx
@@ -1,26 +1,24 @@
-import './Restaurants.css';
-
+import './Restaurants.css'
+// TODO: Jane
 export default function Restaurants() {
-    return (
-        <div className="contentWrapper">
-            <div className="restaurantArea w-full">
-                <div className="flex items-center mb-5">
-                    <p className="title">
-                        熱門餐飲<span className="subtitle hidden lg:inline ml-5"
-                        >饕客必吃美食</span
-                        >
-                    </p>
-                </div>
-                <div className="restaurantCardGroup">
-                    {/* {itmes.map((data) =>
+  return (
+    <div className='contentWrapper'>
+      <div className='restaurantArea w-full'>
+        <div className='flex items-center mb-5'>
+          <p className='title'>
+            熱門餐飲<span className='subtitle hidden lg:inline ml-5'>饕客必吃美食</span>
+          </p>
+        </div>
+        <div className='restaurantCardGroup'>
+          {/* {itmes.map((data) =>
                         <RestaurantCard
                             cardData={data}
                             key={data.ID}
                             className="underLine"
                         />)
                     } */}
-                </div>
-            </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
