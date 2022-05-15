c1dca10ae11a72cf664c7c406183813a56867808
Viaje9 <Viaje9@users.noreply.github.com>
2022-05-15 16:22:33 +0800

    新增onErrorimg utils

---
+++ src/utils/onErrorImg.js
@@ -0,0 +1,53 @@
+import errorImg from '/src/assets/images/empty.svg'
+import { useCallback } from 'react'
+
+function setErrorImg(el) {
+  el.src = errorImg
+  el.style.border = '1px solid'
+  el.style.borderColor = '#E1E1E1'
+}
+
+function setErrorBgImg(el) {
+  el.style.backgroundImage = `url(${errorImg})`
+  el.style.backgroundPosition = 'center'
+  el.style.backgroundSize = 'contain'
+  el.style.backgroundRepeat = 'no-repeat'
+}
+
+export const imgSrc = (src) => {
+  return useCallback((imgRef) => {
+    if (imgRef !== null) {
+      if (src) {
+        const img = new Image()
+        img.src = src
+
+        img.onload = () => {
+          imgRef.src = src
+        }
+        img.onerror = () => setErrorImg(imgRef)
+      }
+
+      if (!src) {
+        setErrorImg(imgRef)
+      }
+    }
+  }, [])
+}
+
+export const bgImgSrc = (src) => {
+  return useCallback((imgRef) => {
+    if (src) {
+      const img = new Image()
+      imgRef.src = src
+
+      img.onload = () => {
+        imgRef.style.backgroundImage = `url(${src})`
+      }
+      img.onerror = () => setErrorBgImg(imgRef)
+    }
+
+    if (!src) {
+      setErrorBgImg(imgRef)
+    }
+  }, [])
+}
--- src/view/Hotel/HotelDetail/HotelDetail.jsx
+++ src/view/Hotel/HotelDetail/HotelDetail.jsx
@@ -1,6 +1,20 @@
 // import Detail from 'src/components/Detail/Detail';
 // TODO: Eric
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react';
+import { fetchHotelAll } from '/src/apis/tourism'
 export default function HotelDetail() {
+  const params = useParams()
+  console.log(params);
+  useEffect(() => {
+    // fetchHotelAll({  $filter: `HotelID eq '${params.id}'` }).then((res) => {
+    //   console.log(res)
+    // })
+    // TODO: filter的參數可以參考response的資料，可能會有HotelID、RestaurantID、ScenicSpotID這部分有問題可以在問
+    // fetchHotelAll({ $top: 5 }).then((res) => {
+    //   console.log(res)
+    // })
+  },[])
   return (
     <div>
       hoteldetail
